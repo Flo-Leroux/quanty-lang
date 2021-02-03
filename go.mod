25fc1ae771ce20fef3c490a824eb60efb172ec84
Florian Leroux <lerouxflorian1@gmail.com>
2021-02-03 21:55:25 +0100

    initial commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module quanty
+
+go 1.15
+
+require (
+	github.com/alecthomas/kong v0.2.12
+	github.com/alecthomas/participle/v2 v2.0.0-alpha3
+	github.com/alecthomas/repr v0.0.0-20201120212035-bb82daffcca2
+)
---
+++ src/Scanner.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"io"
+	"strings"
+)
+
+// Scanner represents a lexical scanner.
+type Scanner struct {
+	r *bufio.Reader
+}
+
+// NewScanner returns new instance of Scanner.
+func NewScanner(r io.Reader) *Scanner {
+	return &Scanner{r: bufio.NewReader(r)}
+}
+
+// Read reads the next rune from the bufferred reader.
+// Returns the rune(0) if an error occurs (or io.EOF is returned).
+func (s *Scanner) read() rune {
+	ch, _, err := s.r.ReadRune()
+	if err != nil {
+		return eof
+	}
+	return ch
+}
+
+// Unread places th previously read rune back on the reader.
+func (s *Scanner) unread() {
+	_ = s.r.UnreadRune()
+}
+
+// scanWhitespace consumes the current rune and all contiguous whitespace.
+func (s *Scanner) scanWhitespace() (tok Token, lit string) {
+	// Create a buffer and read th current character into it.
+	var buf bytes.Buffer
+	buf.WriteRune(s.read())
+
+	// Read every subsequent whitespace character into the buffer.
+	// Non-whitespace characters and EOF will cause the loop to exit.
+	for {
+		if ch := s.read(); ch == eof {
+			break
+		} else if !isWhitespace(ch) {
+			s.unread()
+			break
+		} else {
+			buf.WriteRune(ch)
+		}
+	}
+
+	return WS, buf.String()
+}
+
+// scanIdent consumes the current rune and all contiguous ident runes.
+func (s *Scanner) scanIdent() (tok Token, lit string) {
+	// Create a buffer and read the current character into it.
+	var buf bytes.Buffer
+	buf.WriteRune(s.read())
+
+	// Read every subsequent ident character into the buffer.
+	// Non-ident characters and EOF will cause the loop to exit.
+	for {
+		if ch := s.read(); ch == eof {
+			break
+		} else if !isLetter(ch) && !isDigit(ch) && ch != '_' {
+			s.unread()
+			break
+		} else {
+			_, _ = buf.WriteRune(ch)
+		}
+	}
+
+	// If the string matches a keyword then return that keyword.
+	switch strings.ToUpper(buf.String()) {
+	case "COMPONENT":
+		return COMPONENT, buf.String()
+	case "PROP":
+		return PROP, buf.String()
+	}
+
+	// Otherwise return as a regul identifier.
+	return IDENT, buf.String()
+}
+
+// Scan returns the next token and literal value.
+func (s *Scanner) Scan() (tok Token, lit string) {
+	// Read the next rune.
+	ch := s.read()
+
+	// If we see whitespace then consume all contiguous whitespace.
+	// If we see a letter then consume as an ident or reserved word.
+	if isWhitespace(ch) {
+		s.unread()
+		return s.scanWhitespace()
+	} else if isLetter(ch) {
+		s.unread()
+		return s.scanIdent()
+	}
+
+	// Otherwise read the individual character.
+	switch ch {
+	case eof:
+		return EOF, ""
+	case '}':
+		return CLOSE_BRACKET, string(ch)
+	case '{':
+		return OPEN_BRACKET, string(ch)
+	case '=':
+		return EQUAL, string(ch)
+	case ';':
+		return END, string(ch)
+	case '*':
+		return ASTERISK, string(ch)
+	case ',':
+		return COMMA, string(ch)
+	}
+
+	return ILLEGAL, string(ch)
+}
