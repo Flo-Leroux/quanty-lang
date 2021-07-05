3108c292248293b85cd713456250ea37ff5ee380
Florian Leroux <lerouxflorian1@gmail.com>
2021-07-06 00:02:39 +0200

    refactor(parser): start parse component

--- ast/document.go
+++ ast/document.go
@@ -1,67 +1,68 @@
 package ast
 
-type QueryDocument struct {
-	Operations OperationList
-	Fragments  FragmentDefinitionList
-	Position   *Position `dump:"-"`
-}
+// type QueryDocument struct {
+// 	Operations OperationList
+// 	Fragments  FragmentDefinitionList
+// 	Position   *Position `dump:"-"`
+// }
 
 type SchemaDocument struct {
-	Schema          SchemaDefinitionList
-	SchemaExtension SchemaDefinitionList
-	Directives      DirectiveDefinitionList
-	Definitions     DefinitionList
-	Extensions      DefinitionList
-	Position        *Position `dump:"-"`
+	// Schema          SchemaDefinitionList
+	// SchemaExtension SchemaDefinitionList
+	// Directives      DirectiveDefinitionList
+	Definitions DefinitionList `json:"components"`
+	// Extensions      DefinitionList
+	Position *Position `dump:"-" json:"-"`
 }
 
 func (d *SchemaDocument) Merge(other *SchemaDocument) {
-	d.Schema = append(d.Schema, other.Schema...)
-	d.SchemaExtension = append(d.SchemaExtension, other.SchemaExtension...)
-	d.Directives = append(d.Directives, other.Directives...)
+	// d.Schema = append(d.Schema, other.Schema...)
+	// d.SchemaExtension = append(d.SchemaExtension, other.SchemaExtension...)
+	// d.Directives = append(d.Directives, other.Directives...)
 	d.Definitions = append(d.Definitions, other.Definitions...)
-	d.Extensions = append(d.Extensions, other.Extensions...)
+	// d.Extensions = append(d.Extensions, other.Extensions...)
 }
 
 type Schema struct {
-	Query        *Definition
-	Mutation     *Definition
-	Subscription *Definition
+	Components map[string]*Definition
+	// Query        *Definition
+	// Mutation     *Definition
+	// Subscription *Definition
 
-	Types      map[string]*Definition
-	Directives map[string]*DirectiveDefinition
+	// Types      map[string]*Definition
+	// Directives map[string]*DirectiveDefinition
 
-	PossibleTypes map[string][]*Definition
-	Implements    map[string][]*Definition
+	// PossibleTypes map[string][]*Definition
+	// Implements    map[string][]*Definition
 }
 
-func (s *Schema) AddPossibleType(name string, def *Definition) {
-	s.PossibleTypes[name] = append(s.PossibleTypes[name], def)
-}
+// func (s *Schema) AddPossibleType(name string, def *Definition) {
+// 	s.PossibleTypes[name] = append(s.PossibleTypes[name], def)
+// }
 
 // GetPossibleTypes will enumerate all the definitions for a given interface or union
-func (s *Schema) GetPossibleTypes(def *Definition) []*Definition {
-	return s.PossibleTypes[def.Name]
-}
+// func (s *Schema) GetPossibleTypes(def *Definition) []*Definition {
+// 	return s.PossibleTypes[def.Name]
+// }
 
-func (s *Schema) AddImplements(name string, iface *Definition) {
-	s.Implements[name] = append(s.Implements[name], iface)
-}
+// func (s *Schema) AddImplements(name string, iface *Definition) {
+// 	s.Implements[name] = append(s.Implements[name], iface)
+// }
 
 // GetImplements returns all the interface and union definitions that the given definition satisfies
-func (s *Schema) GetImplements(def *Definition) []*Definition {
-	return s.Implements[def.Name]
-}
+// func (s *Schema) GetImplements(def *Definition) []*Definition {
+// 	return s.Implements[def.Name]
+// }
 
-type SchemaDefinition struct {
-	Description    string
-	Directives     DirectiveList
-	OperationTypes OperationTypeDefinitionList
-	Position       *Position `dump:"-"`
-}
+// type SchemaDefinition struct {
+// 	Description    string
+// 	Directives     DirectiveList
+// 	OperationTypes OperationTypeDefinitionList
+// 	Position       *Position `dump:"-" json:"-"`
+// }
 
-type OperationTypeDefinition struct {
-	Operation Operation
-	Type      string
-	Position  *Position `dump:"-"`
-}
+// type OperationTypeDefinition struct {
+// 	Operation Operation
+// 	Type      string
+// 	Position  *Position `dump:"-" json:"-"`
+// }
--- go.mod
+++ go.mod
@@ -4,10 +4,10 @@ go 1.15
 
 require (
 	github.com/alecthomas/kong v0.2.12
-	github.com/alecthomas/participle v0.7.1
-	github.com/alecthomas/participle/v2 v2.0.0-alpha3
+	github.com/alecthomas/participle v0.7.1 // indirect
+	github.com/alecthomas/participle/v2 v2.0.0-alpha3 // indirect
 	github.com/alecthomas/repr v0.0.0-20201120212035-bb82daffcca2
 	github.com/iancoleman/strcase v0.1.3 // indirect
-	github.com/vektah/gqlparser v1.3.1
+	github.com/vektah/gqlparser v1.3.1 // indirect
 	github.com/vektah/gqlparser/v2 v2.2.0
 )
