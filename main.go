9d7a2b6ae9ff5cc4e419611ed23afe97ca6740a8
Florian Leroux <lerouxflorian1@gmail.com>
2021-07-12 23:07:18 +0200

    refactor: add cli support

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"quanty/ast"
+	. "quanty/cli"
 	"quanty/parser"
 )
 
@@ -14,84 +16,27 @@ func check(e error) {
 	}
 }
 
-// var schema = gqlparser.MustLoadSchema(
-// 	&gqlast.Source{
-// 		Name: "test.graphql",
-// 		Input: `
-// 		type User {
-// 			id: ID!
-// 			name: String!
-// 			photo: Picture
-// 		}
-
-// 		type Picture {
-// 			id: ID!
-// 			url: String!
-// 		}
-// 		`,
-// 	},
-// )
-
-var schema, err = parser.ParseFile(
-	&ast.Source{
-		Name: "component.qy",
-		Input: `
-			module main
-
-			import (
-				Button "./button.qy"
-				Cpt "./captain.qy"
-			)
-
-			component Main {
-				div(class: "bg-red-500") {
-					"hello"
-				}
-			}
-		`,
-	},
-)
-
 func main() {
 
-	var dumped string
-	if err != nil {
-		dumped = ast.Dump(err)
-	} else {
-		dumped = ast.Dump(schema)
+	cli := NewCli()
+
+	for _, file := range cli.Files {
+		fileRead, errRead := ioutil.ReadFile(file)
+		check(errRead)
+
+		fileParsed, errParsed := parser.ParseFile(
+			&ast.Source{
+				Name:  file,
+				Input: string(fileRead),
+			},
+		)
+
+		var dumped string
+		if errParsed != nil {
+			dumped = ast.Dump(errParsed)
+		} else {
+			dumped = ast.Dump(fileParsed)
+		}
+		fmt.Print(dumped)
 	}
-	fmt.Print(dumped)
-
-	// for _, t := range schema.Types {
-	// 	fmt.Println(t.Name)
-	// }
-
-	// cli := cli.NewCli()
-	// qm := &ast.File{}
-
-	// for _, file := range cli.Files {
-	// 	res, err := ioutil.ReadFile(file)
-	// 	check(err)
-
-	// 	err = parser.ParseString("", string(res), qm)
-	// 	check(err)
-
-	// 	toPrint, err := json.MarshalIndent(qm, "", "    ")
-	// 	check(err)
-	// 	fmt.Println(string(toPrint))
-	// 	// repr.Println(qm, repr.Indent("  "), repr.OmitEmpty(true))
-	// }
-
-	// 	err = parser.ParseString("", `
-	// package main
-
-	// component Main {
-	// 	prop name string = "test"
-	// }
-	// `, qm)
-
-	// 	if err != nil {
-	// 		panic(err)
-	// 	}
-
 }
