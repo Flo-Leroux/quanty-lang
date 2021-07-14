9afb984ee7fdb6fd1459b66068c20296a01d917b
Florian Leroux <lerouxflorian1@gmail.com>
2021-07-14 20:36:23 +0200

    feat(cmd): check files wth .qy extension

---
+++ cmd/helpers.go
@@ -0,0 +1,26 @@
+package cmd
+
+import (
+	"log"
+	"os"
+	"path/filepath"
+)
+
+const (
+	extension = ".qy"
+)
+
+func findFiles(files *[]string) filepath.WalkFunc {
+	return func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if filepath.Ext(path) != extension {
+			return nil
+		}
+
+		*files = append(*files, path)
+		return nil
+	}
+}
